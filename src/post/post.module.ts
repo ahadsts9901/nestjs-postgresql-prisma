f9c805bef845ebca578822d13cad30f76c69896d
ahadsts9901 <ahadsts990@gmail.com>
2024-08-08 16:32:43 +0500

    cod

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
-import { PostService } from './post.service';
+import { PostsService } from './post.service';
 
 @Module({
   controllers: [PostController],
-  providers: [PostService]
+  providers: [PostsService]
 })
 export class PostModule {}
--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PostService } from './post.service';
+import { PostsService } from './post.service';
 
 describe('PostService', () => {
-  let service: PostService;
+  let service: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [PostsService],
     }).compile();
 
-    service = module.get<PostService>(PostService);
+    service = module.get<PostsService>(PostsService);
   });
 
   it('should be defined', () => {
